565169f629f5521f8b8db0b75a03073f01cdbc78
Shyam Prag <pragshyam11@gmail.com>
2021-06-09 22:58:32 +0200

    Address entity

---
+++ src/main/java/za/ac/cput/entity/Address.java
@@ -0,0 +1,53 @@
+package za.ac.cput.entity;
+
+public class Address {
+    private final String street;
+    private final String houseNumber;
+    private final String zipCode;
+    private final String city;
+
+    //getters
+    public static class Builder {
+        private String street;
+        private String houseNumber;
+        private String zipCode;
+        private String city;
+
+        public Builder() {
+
+        }
+
+        public Builder withStreet(String street) {
+            this.street = street;
+            return this;
+        }
+
+        public Builder withHouseNumber(String houseNumber) {
+            this.houseNumber = houseNumber;
+            return this;
+        }
+
+        public Builder withZipCode(String zipCode) {
+            this.zipCode = zipCode;
+            return this;
+        }
+
+        public Builder city(String city) {
+            this.city = city;
+            return this;
+
+        }
+        public Address build(){
+            return new Address(this);
+        }
+    }
+    private Address(Builder builder){
+        this.street=builder.street;
+        this.houseNumber=builder.houseNumber;
+        this.zipCode=builder.zipCode;
+        this.city=builder.city;
+    }
+
+
+
+}
