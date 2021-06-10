5feba99ed6fb28e25a7966e91ec6e007cf0d2877
Cameron Noemdo <219115443@mycput.ac.za>
2021-06-11 00:53:05 +0200

    Factory class for Customer entity

---
+++ src/main/java/za/ac/cput/factory/CustomerFactory.java
@@ -0,0 +1,20 @@
+package za.ac.cput.factory;
+
+import za.ac.cput.entity.Customer;
+
+/*  CustomerFactory.java
+    Factory for the Customer
+    Author: Cameron Noemdo 219115443
+    Date: 10/06/21
+ */
+
+public class CustomerFactory
+{
+    public static Customer createCustomer (String firstName, String lastName)
+    {
+        return new Customer.Builder()
+            .setFirstName(firstName)
+            .setLastName(lastName)
+            .build();
+    }
+}
