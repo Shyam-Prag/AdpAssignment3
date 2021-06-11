f7f32007086751533098de232a96d39ad9843721
Cameron Noemdo <219115443@mycput.ac.za>
2021-06-11 17:26:34 +0200

    Created and added test class for Customer factory

---
+++ src/test/java/za/ac/cput/factory/CustomerFactoryTest.java
@@ -0,0 +1,68 @@
+package za.ac.cput.factory;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Timeout;
+import za.ac.cput.entity.Customer;
+
+import java.util.concurrent.TimeUnit;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/*  CustomerFactoryTest.java
+    Test class for Student
+    Author: Cameron Noemdo 219115443
+    Date: 10/06/21
+ */
+
+class CustomerFactoryTest
+{
+    Customer cus1,cus2,cus3;
+
+    @BeforeEach
+    void setup()
+    {
+        cus1=CustomerFactory.createCustomer("John", "Doe");
+        cus2=CustomerFactory.createCustomer("Susan", "Mary");
+        cus3=cus1;
+    }
+
+    @Test
+    void testCustomerIdentity()
+    {
+        assertSame(cus1,cus3);
+        System.out.println("Objects are identical.");
+    }
+
+    @Test
+    void testCustomerEquality()
+    {
+        assertEquals(cus1, cus3);
+        System.out.println("Objects are equal.");
+    }
+
+    @Test
+    @Timeout(1)
+    void testCustomerTimeout()
+    {
+        try
+        {
+            TimeUnit.SECONDS.sleep(2);
+            System.out.println("Timeout test PASSED");
+        }
+        catch(InterruptedException iEx)
+        {
+            System.out.println("Exception: "+iEx.getMessage());
+            System.out.println("Test timed out");
+        }
+    }
+
+    @Test
+    @Disabled("Test temporarily disabled due to maintenance.")
+    void testCustomerDisabled()
+    {
+        assertEquals(cus1, cus2);
+        System.out.println("Objects are equal.");
+    }
+}
