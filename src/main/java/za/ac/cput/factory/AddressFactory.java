1bbbe9e2b41ef605ee9ebc73343960ba6c8a4da2
Shyam Prag <pragshyam11@gmail.com>
2021-06-11 14:37:03 +0200

    updated test classes

--- src/main/java/za/ac/cput/factory/AddressFactory.java
+++ src/main/java/za/ac/cput/factory/AddressFactory.java
@@ -4,17 +4,26 @@ import za.ac.cput.entity.Address;
 
 public class AddressFactory {
     public static Address buildAddress(
-            String street,
+
             String houseNumber,
             String zipCode,
             String city
-    ) {
+    )
+
+    {
+        if (houseNumber.isEmpty()||zipCode.isEmpty()||city.isEmpty())
+
+        return null;
+        int rand = (int)(Math.random()*(1000-1+1)+1);
+        String addressStreet ="AS"+Integer.toString(rand)+ houseNumber.charAt(0);
+
         return new Address.Builder()
-                .setStreet(street)
+                .setStreet(addressStreet)
                 .setHouseNumber(houseNumber)
                 .setZipCode(zipCode)
                 .setCity(city)
                 .build();
     }
+
 }
 
