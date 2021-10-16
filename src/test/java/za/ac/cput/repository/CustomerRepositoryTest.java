05c67a206bdacbf3c7d1a1a8624fa683d93249c0
Cameron Noemdo <219115443@mycput.ac.za>
2021-10-16 21:01:27 +0200

    Created customer repository test class

---
+++ src/test/java/za/ac/cput/repository/CustomerRepositoryTest.java
@@ -0,0 +1,11 @@
+package za.ac.cput.repository;
+
+import org.junit.jupiter.api.Test;
+import za.ac.cput.entity.Customer;
+import za.ac.cput.factory.CustomerFactory;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CustomerRepositoryTest {
+
+}
