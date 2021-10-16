b91011264c3d7ea45fa27e6bc6c26c08d0f96c69
Cameron Noemdo <219115443@mycput.ac.za>
2021-10-16 20:51:20 +0200

    Completed customer repository

--- src/main/java/za/ac/cput/repository/CustomerRepository.java
+++ src/main/java/za/ac/cput/repository/CustomerRepository.java
@@ -1,4 +1,51 @@
 package za.ac.cput.repository;
 
-public class CustomerRepository {
+import za.ac.cput.entity.Customer;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CustomerRepository implements IRepository <Customer,String>{
+    private List<Customer> customerDB;
+
+    public CustomerRepository() {
+        this.customerDB = new ArrayList<>();
+    }
+
+    @Override
+    public Customer create(Customer customer) {
+        this.customerDB.add(customer);
+        return customer;
+    }
+
+    @Override
+    public Customer read(String id) {
+        return this.customerDB.stream().filter(c -> c.getId().equalsIgnoreCase(id)).findAny().orElse(null);
+    }
+
+    @Override
+    public Customer update(Customer customer) {
+        Customer oldCustomer=read(customer.getId());
+        if(oldCustomer!=null)
+        {
+            customerDB.remove(oldCustomer);
+            customerDB.add(customer);
+        }
+        return customer;
+    }
+
+    @Override
+    public boolean delete(String id) {
+        Customer customer=read(id);
+        return this.customerDB.remove(customer);
+    }
+
+    public List<Customer> getAll()
+    {
+        return this.customerDB;
+    }
+
+    public void deleteAll(){
+        this.customerDB.clear();
+    }
 }
