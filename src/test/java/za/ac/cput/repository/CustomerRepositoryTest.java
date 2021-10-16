2b0775dbcef5ff7382043243a743bbaad91b7e15
Cameron Noemdo <219115443@mycput.ac.za>
2021-10-16 21:24:34 +0200

    Completed create, read, and delete test cases for customer repository

--- src/test/java/za/ac/cput/repository/CustomerRepositoryTest.java
+++ src/test/java/za/ac/cput/repository/CustomerRepositoryTest.java
@@ -7,5 +7,32 @@ import za.ac.cput.factory.CustomerFactory;
 import static org.junit.jupiter.api.Assertions.*;
 
 class CustomerRepositoryTest {
+    private static final CustomerRepository customerRepository=new CustomerRepository();
+    private static final Customer customer= CustomerFactory.createCustomer("Cameron","Noemdo");
 
+    @Test
+    void create() {
+        Customer customer1=customerRepository.create(customer);
+        assertEquals(customer1.getId(),customer.getId());
+        System.out.println("Created customer: "+customer1);
+    }
+
+    @Test
+    void read() {
+        Customer customer1=customerRepository.read(customer.getId());
+        assertEquals(customer1.getId(),customer.getId());
+        System.out.println("Read customer: "+customer1);
+    }
+
+    @Test
+    void update() {
+
+    }
+
+    @Test
+    void delete() {
+        customerRepository.delete(customer.getId());
+        System.out.println("Deleted customer "+customer.getId());
+        assertNull(customer);
+    }
 }
