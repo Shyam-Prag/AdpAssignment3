a9139a7ff2b42f92b11ab4da49fc086695944b7c
Cameron Noemdo <219115443@mycput.ac.za>
2021-10-19 13:36:45 +0200

    Completed customer service

--- src/main/java/za/ac/cput/service/CustomerService.java
+++ src/main/java/za/ac/cput/service/CustomerService.java
@@ -1,4 +1,47 @@
 package za.ac.cput.service;
 
-public class CustomerService {
+import za.ac.cput.entity.Customer;
+import za.ac.cput.repository.CustomerRepository;
+
+import java.util.List;
+
+public class CustomerService implements ICustomerService{
+    private static CustomerService customerService=null;
+
+    private CustomerRepository customerRepository;
+
+    @Override
+    public List<Customer> getAllCustomers() {
+        return customerRepository.findAll();
+    }
+
+    @Override
+    public Customer create(Customer customer) {
+        return this.customerRepository.save(customer);
+    }
+
+    @Override
+    public Customer read(String s) {
+        return this.customerRepository.findById(s).orElse(null);
+    }
+
+    @Override
+    public Customer update(Customer customer) {
+        if(this.customerRepository.existsById(customer.getId()))
+        {
+            return this.customerRepository.save(customer);
+        }
+        else
+        {
+            return null;
+        }
+    }
+
+    @Override
+    public boolean delete(String s) {
+        this.customerRepository.deleteById(s);
+        if (this.customerRepository.existsById(s))
+            return false;
+        return true;
+    }
 }
