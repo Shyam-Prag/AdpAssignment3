cd64f9ab05ca6d9711dba5f65003621e6ad46b7c
Cameron Noemdo <219115443@mycput.ac.za>
2021-10-19 13:47:51 +0200

    Completed customer controller

--- src/main/java/za/ac/cput/controllers/CustomerController.java
+++ src/main/java/za/ac/cput/controllers/CustomerController.java
@@ -1,4 +1,37 @@
 package za.ac.cput.controllers;
 
+import za.ac.cput.entity.Customer;
+import za.ac.cput.factory.CustomerFactory;
+import za.ac.cput.service.CustomerService;
+
+import java.util.List;
+
 public class CustomerController {
+    private CustomerService customerService;
+
+    public Customer create(Customer customer)
+    {
+        Customer customer1= CustomerFactory.createCustomer(customer.getFirstName(), customer.getLastName());
+        return customerService.create(customer1);
+    }
+
+    public Customer read(String id)
+    {
+        return customerService.read(id);
+    }
+
+    public Customer update(Customer customer)
+    {
+        return customerService.update(customer);
+    }
+
+    public boolean delete(String id)
+    {
+        return customerService.delete(id);
+    }
+
+    public List<Customer> getAll()
+    {
+        return customerService.getAllCustomers();
+    }
 }
