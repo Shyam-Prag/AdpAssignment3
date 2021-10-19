6fdd3f77267322e5f29f0c5e8c7a80b9ee9a640f
Cameron Noemdo <219115443@mycput.ac.za>
2021-10-19 13:56:34 +0200

    Added customer repository annotation

--- src/main/java/za/ac/cput/repository/CustomerRepository.java
+++ src/main/java/za/ac/cput/repository/CustomerRepository.java
@@ -1,7 +1,9 @@
 package za.ac.cput.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import za.ac.cput.entity.Customer;
 
+@Repository
 public interface CustomerRepository extends JpaRepository<Customer,String> {
 }
