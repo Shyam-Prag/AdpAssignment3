06a1ab3f6b44d6dd7dc2829c2b0181e4e880576e
Cameron Noemdo <219115443@mycput.ac.za>
2021-10-19 13:25:44 +0200

    Updated customer factory

--- src/main/java/za/ac/cput/factory/CustomerFactory.java
+++ src/main/java/za/ac/cput/factory/CustomerFactory.java
@@ -2,6 +2,8 @@ package za.ac.cput.factory;
 
 import za.ac.cput.entity.Customer;
 
+import java.util.UUID;
+
 /*  CustomerFactory.java
     Factory for the Customer
     Author: Cameron Noemdo 219115443
@@ -12,7 +14,9 @@ public class CustomerFactory
 {
     public static Customer createCustomer (String firstName, String lastName)
     {
+        String id= UUID.randomUUID().toString(); //class needed to generate a unique ID
         return new Customer.Builder()
+                .setID(id)
                 .setFirstName(firstName)
                 .setLastName(lastName)
                 .build();
