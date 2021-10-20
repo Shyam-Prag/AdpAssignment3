5ce31236a9234aea7e9f72b47e6c901f5e4372ac
Cameron Noemdo <219115443@mycput.ac.za>
2021-10-20 16:32:21 +0200

    Updated customer controller

--- src/main/java/za/ac/cput/controllers/CustomerController.java
+++ src/main/java/za/ac/cput/controllers/CustomerController.java
@@ -1,36 +1,45 @@
 package za.ac.cput.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 import za.ac.cput.entity.Customer;
 import za.ac.cput.factory.CustomerFactory;
 import za.ac.cput.service.CustomerService;
 
 import java.util.List;
 
+@RestController
+@RequestMapping("/customer")
 public class CustomerController {
-
+    @Autowired
     private CustomerService customerService;
 
+    @PostMapping("/create")
     public Customer create(Customer customer)
     {
         Customer newCustomer= CustomerFactory.createCustomer(customer.getFirstName(), customer.getLastName());
         return customerService.create(newCustomer);
     }
 
+    @GetMapping("/read/{id}")
     public Customer read(String id)
     {
         return customerService.read(id);
     }
 
+    @PostMapping("/update")
     public Customer update(Customer customer)
     {
         return customerService.update(customer);
     }
 
+    @DeleteMapping("/delete/{id}")
     public boolean delete(String id)
     {
         return customerService.delete(id);
     }
 
+    @GetMapping("/getall")
     public List<Customer> getAll()
     {
         return customerService.getAll();
