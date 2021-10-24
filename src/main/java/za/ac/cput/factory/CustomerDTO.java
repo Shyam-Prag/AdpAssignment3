9b762099659f2f5b1365fdbe6516076ee2986739
Cameron Noemdo <219115443@mycput.ac.za>
2021-10-24 15:58:28 +0200

    Added Customer DTO

---
+++ src/main/java/za/ac/cput/factory/CustomerDTO.java
@@ -0,0 +1,21 @@
+package za.ac.cput.factory;
+
+public class CustomerDTO {
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    private String firstName,lastName;
+}
